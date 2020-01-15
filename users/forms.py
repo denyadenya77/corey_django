3370c27259a24ff63f5a4210bfb1a9552d1870e9
denyadenya77 <jetropak7@gmail.com>
2020-01-15 16:34:12 +0200

    first_name and last_name added

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)  # required=True - обязательное поле
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
