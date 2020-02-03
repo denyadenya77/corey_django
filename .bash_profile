ad0da52af0e9da5f291e8b5fd49d978afd984b5f
denyadenya77 <jetropak7@gmail.com>
2020-02-03 14:25:55 +0200

    AWS S3 added + environ vars set

---
+++ .bash_profile
@@ -0,0 +1,13 @@
+#!/bin/bash
+# DJANGO BLOG VARS
+
+export BLOG_SECRET_KEY='h$^a9^*um7%^_ne8^xv%d58eo$^o*pt!h$ps$^p3ru8*==#0k-'
+export BLOG_DEBUG_VALUE='True'
+
+export AWS_ACCESS_KEY_ID='AKIAV7XTHWSRR2S24TUJ'
+export AWS_SECRET_ACCESS_KEY='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'
+export AWS_STORAGE_BUCKET_NAME='denys-django-blog-files'
+
+
+# EMAIL VARS
+export EMAIL_HOST_PASSWORD='denysdenys77'
--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h$^a9^*um7%^_ne8^xv%d58eo$^o*pt!h$ps$^p3ru8*==#0k-'
+# SECRET_KEY = 'h$^a9^*um7%^_ne8^xv%d58eo$^o*pt!h$ps$^p3ru8*==#0k-'
+SECRET_KEY = os.environ.get('BLOG_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('BLOG_DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blogpostdjangoapp.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -139,10 +143,18 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'v.vasilyev321@gmail.com'
-EMAIL_HOST_PASSWORD = 'denysdenys77'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-
+AWS_S3_REGION_NAME = "eu-north-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
